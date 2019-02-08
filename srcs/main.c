131f70921f3aecd66a95760997e802eda8db6ab0
Maxime GOUIN <magouin@e3r12p19.42.fr>
2019-02-08 21:22:43 +0100

    ca avance

--- srcs/main.c
+++ srcs/main.c
@@ -35,12 +35,13 @@ int8_t	init_glfw(t_d *data)
 	glfwWindowHint(GLFW_SAMPLES, 3);
 	glfwSetErrorCallback(error_callback);
 
-	data->window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+	data->window = glfwCreateWindow(1600, 900, "Hello World", NULL, NULL);
 	if (!data->window)
 	{
 		glfwTerminate();
 		return (0);
 	}
+	glfwSetInputMode(data->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	// glfwSetFramebufferSizeCallback(data->window, resize_callback);
 	glfwMakeContextCurrent(data->window);
 	glfwGetWindowSize(data->window, &data->width, &data->height);
@@ -80,13 +81,21 @@ void	key_event(t_d *data, uint delta)
 {
 	// t_mat4x4 rotation;
 	t_vec3	transl;
-	float depl;
-	float rot;
+	static float depl = 1;
+	static float rot = 30;
 
-	depl = 10;
-	rot = 30;
 	if (glfwGetKey(data->window, GLFW_KEY_ESCAPE))
 		glfwSetWindowShouldClose(data->window, true);
+	if (glfwGetKey(data->window, GLFW_KEY_MINUS))
+	{
+		printf("minus\n");
+		depl /= 1.01;
+	}
+	if (glfwGetKey(data->window, GLFW_KEY_EQUAL))
+	{
+		printf("plus\n");
+		depl *= 1.01;
+	}
 	if (glfwGetKey(data->window, GLFW_KEY_W))
 	{
 		printf("W\n");
@@ -149,6 +158,36 @@ void	key_event(t_d *data, uint delta)
 	ft_mat4x4_to_float_array(data->cam.view_f, data->cam.view);
 }
 
+void rotation_cam(t_d *data, uint delta)
+{
+	double			x;
+	double			y;
+	static double	last_x = 0;
+	static double	last_y = 0;
+	static float	roty = 0;
+	float			rotx;
+	t_vec3			tmp;
+
+	glfwGetCursorPos(data->window, &x, &y);
+	roty += (y - last_y) * delta / 1000;
+	// roty = (int)y - (int)last_y;
+	rotx = (last_x - x);
+	printf("roty = %f\n", roty);
+
+	data->dir = ft_vec3_rotate(data->dir, rotx * delta / 1000, ft_vec3_cross(ft_vec3_cross(data->dir, (t_vec3){0, 1, 0}), data->dir));
+	tmp = (t_vec3){data->dir.x, 0, data->dir.z};
+	ft_vec3_normalize(&tmp);
+	if (roty < 90 && roty > -90)
+		data->dir = ft_vec3_rotate(tmp, roty, ft_vec3_cross(data->dir, (t_vec3){0, 1, 0}));
+
+		// data->dir = ft_vec3_rotate((t_vec3){0, 0, -1}, roty, ft_vec3_cross(data->dir, (t_vec3){0, 1, 0}));
+	else
+		roty = roty >= 90 ? 90 : -90;
+	ft_mat4x4_set_look_at(data->cam.view, data->eye, ft_vec3_sub(data->eye, data->dir), data->cam.up);
+	last_y = y;
+	last_x = x;
+}
+
 int main(int ac, char **av)
 {
 	t_d				data;
@@ -159,7 +198,11 @@ int main(int ac, char **av)
 	uint			j;
 	uint			i;
 	unsigned int	*texs;
-
+	GLuint ebo = 0;
+	GLuint vao = 0;
+	uint	i_tex;
+	int x;
+	int nb_face;
 
 	i = 0;
 	init_all(&data);
@@ -198,17 +241,7 @@ int main(int ac, char **av)
 	printf("nb = %zu\n", data.texture_nbr);
 	data.imgs = malloc(sizeof(t_img) * data.texture_nbr);
 	texs = malloc(sizeof(uint) * data.texture_nbr); 
-	// glGenTextures(1, texs);
 	glGenTextures(data.texture_nbr, texs);
-	
-	// uint n = 0;
-	// while (n < data.texture_nbr)
-	// {
-	// 	printf("%d\n", texs[n]);
-	// 	n++;
-	// }
-	
-	uint	i_tex;
 
 	 unsigned int *texture1;
 	 texture1 = malloc(sizeof(unsigned int) * 4);
@@ -229,15 +262,8 @@ int main(int ac, char **av)
 				glBindTexture(GL_TEXTURE_2D, texs[i_tex]);
 				create_image_from_png(&data, i_tex, obj[i].mtls[j].img_file);
 
-				// printf("w = %d, h = %d\n", data.imgs[i_tex].w, data.imgs[i_tex].h);
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, data.imgs[i_tex].w, data.imgs[i_tex].h, 0, GL_BGRA, GL_UNSIGNED_BYTE, data.imgs[i_tex].data);
-				// printf("-\n");
 				glGenerateMipmap(GL_TEXTURE_2D);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-				// printf("*\n");
 				i_tex++;
 			}
 			j++;
@@ -245,59 +271,7 @@ int main(int ac, char **av)
 		i++;
 	}
 
-	// glGenTextures(2, texture1);
- //    glBindTexture(GL_TEXTURE_2D, texture1[0]); 
- //     // set the texture wrapping parameters
- //    // set texture filtering parameters
- //    // load image, create texture and generate mipmaps
-
- //    create_image_from_png(&data, 0, obj[0].mtls[0].img_file);
- //    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, data.imgs[0].w, data.imgs[0].h, 0, GL_RGB, GL_UNSIGNED_BYTE, data.imgs[0].data);
- //    glGenerateMipmap(GL_TEXTURE_2D);
-
- //    glBindTexture(GL_TEXTURE_2D, texture1[1]); 
- //     // set the texture wrapping parameters
- //    // set texture filtering parameters
- //    // load image, create texture and generate mipmaps
-
- //    create_image_from_png(&data, 1, obj[0].mtls[1].img_file);
- //    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, data.imgs[1].w, data.imgs[1].h, 0, GL_RGB, GL_UNSIGNED_BYTE, data.imgs[1].data);
- //    glGenerateMipmap(GL_TEXTURE_2D);
-
- //    glBindTexture(GL_TEXTURE_2D, texture1[2]); 
- //     // set the texture wrapping parameters
- //    // set texture filtering parameters
- //    // load image, create texture and generate mipmaps
-
- //    create_image_from_png(&data, 2, obj[0].mtls[2].img_file);
- //    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, data.imgs[2].w, data.imgs[2].h, 0, GL_RGB, GL_UNSIGNED_BYTE, data.imgs[2].data);
- //    glGenerateMipmap(GL_TEXTURE_2D);
-
- //    glBindTexture(GL_TEXTURE_2D, texture1[3]); 
- //     // set the texture wrapping parameters
- //    // set texture filtering parameters
- //    // load image, create texture and generate mipmaps
-
- //    create_image_from_png(&data, 3, obj[0].mtls[3].img_file);
- //    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, data.imgs[3].w, data.imgs[3].h, 0, GL_RGB, GL_UNSIGNED_BYTE, data.imgs[3].data);
- //    glGenerateMipmap(GL_TEXTURE_2D);
-
-
-
-	// create_image_from_png(&data, 0, "textures/Cat_diffuse.png");
-	// create_image_from_png(&data, 0, "textures/Cat_diffuse.png");
-
-	// int q = 0;
-	// while (q < obj[0].indices_nbr)
-	// {
-	// 	printf("#%d: %f %f\n", q, obj[0].textures[q].x, obj[0].textures[q].y);
-	// 	q++;
-	// }
-
-	GLuint vao = 0;
 	glGenVertexArrays(1, &vao);
-	GLuint ebo = 0;
-	GLuint tex = 0;
 	glGenBuffers(1, &ebo);
 	glGenBuffers(1, &data.buffer[0]);
 	glBindVertexArray(vao);
@@ -314,25 +288,15 @@ int main(int ac, char **av)
 
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-	// rot = 180;
-	// ft_mat4x4_set_rotation(model, 0, (t_vec3){1, 0, 0});
-	// ft_mat4x4_set_translation(model, (t_vec3){0, 0, 1});
 	data.eye = (t_vec3){0, 0, -10};
 	data.dir = (t_vec3){0, 0, -1};
 
 	ft_mat4x4_set_identity(data.cam.model);
 	ft_mat4x4_to_float_array(data.cam.model_f, data.cam.model);
 
-
-
-	// dir = ft_vec4_to_vec3(ft_mat4x4_mult_with_vec4(model, (t_vec4){0, 0, 0, 1}));
 	data.cam.up = (t_vec3){0,1,0};
 	ft_mat4x4_set_look_at(data.cam.view, data.eye, ft_vec3_sub(data.eye, data.dir), data.cam.up);
-	// ft_mat4x4_translate(data.cam.view, (t_vec3){0, 0, -50});
 	ft_mat4x4_to_float_array(data.cam.view_f, data.cam.view);
-
 
 	ft_mat4x4_set_projection(data.cam.proj, (float[]){45, (float)data.width / data.height, .1f, 1000.0f});
 	ft_mat4x4_to_float_array(data.cam.proj_f, data.cam.proj);
@@ -345,52 +309,29 @@ int main(int ac, char **av)
 	gettimeofday(&tp, NULL);
 	last = (tp.tv_sec * 1000 + tp.tv_usec / 1000);
 
-	// t_vec3			*tex_vertices;
-	// int				tex_vertices_nbr;
-	// int				tex_vertices_curr;
-
-	// int q = 0;
-	// while (q < obj.tex_vertices_nbr)
-	// {
-	// 	printf("vt #%d: %f %f %f\n", q, obj.tex_vertices[q].x, obj.tex_vertices[q].y, obj.tex_vertices[q].z);
-	// 	q++;
-	// }
-
-	printf("tex = %d\n", texs[0]);
 	while (!glfwWindowShouldClose(data.window))
 	{
 		delta = (tp.tv_sec * 1000 + tp.tv_usec / 1000) - last;
 		last = (tp.tv_sec * 1000 + tp.tv_usec / 1000);
 
 		key_event(&data, delta);
+		rotation_cam(&data, delta);
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(data.program);
-
-
 		glActiveTexture(GL_TEXTURE0);
 		glBindVertexArray(vao);
-
-		// ft_mat4x4_set_rotation(cam.model, rot + 90 * delta / 1000.0, (t_vec3){1, 0, 0});
-		// // ft_mat4x4_scale(cam.model, (t_vec3){0.5, 0.5, 0.5});
-		// // ft_mat4x4_translate(cam.model, (t_vec3){0, 0, 0});
-		// ft_mat4x4_to_float_array(cam.model_f, cam.model);
-		// rot += 90 * delta / 1000.0;
 
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, data.cam.model_f);
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, data.cam.view_f);
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, data.cam.proj_f);
 
-
-		int x;
-
 		x = 0;
 		while (x < obj[0].mtl_nbr)
 		{
-			glBindTexture(GL_TEXTURE_2D, texs[x]);
-			int nb_face;
-
+			if (obj[0].mtls[x].img_file)
+				glBindTexture(GL_TEXTURE_2D, texs[x]);
 			if (x + 1 == obj[0].mtl_nbr)
 				nb_face = obj[0].faces_nbr - obj[0].mtls[x].index;
 			else
