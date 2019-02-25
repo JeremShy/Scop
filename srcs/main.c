283d08d6e0f289b886316046177757e124848076
Maxime GOUIN <magouin@e3r12p19.42.fr>
2019-02-25 18:35:30 +0100

    push Final

--- srcs/main.c
+++ srcs/main.c
@@ -1,9 +1,10 @@
+
 #include <scop.h>
 #include <cglm/cglm.h>   /* for inline */
 
 #define STB_IMAGE_IMPLEMENTATION
 
-struct s_key_event keys[] = {	
+struct s_key_event keys[] = {
 	{GLFW_KEY_G, NULL},
 	{GLFW_KEY_T, t},
 	{GLFW_KEY_V, v},
@@ -30,57 +31,44 @@ struct s_key_event keys[] = {
 	{GLFW_KEY_UP, key_up},
 	{GLFW_KEY_DOWN, key_down},
 	{GLFW_KEY_RIGHT, key_right},
-	{GLFW_KEY_LEFT, key_left},
-								};
+	{GLFW_KEY_LEFT, key_left}};
 
 int8_t	init_data(t_d *data)
 {
 	ft_bzero(data, sizeof(t_d));
 	data->depl = 1;
-	data->ambient = 0.5;
-	data->lightColor = (t_vec3){1,1,1};
-	data->lightPos = (t_vec3){50,0,50};
+	data->ambient = 0.1;
+	data->lightColor = (t_vec3){1, 1, 1};
+	data->lightPos = (t_vec3){0, 0, 50};
+	data->drawing_mode = 0;
 	return (1);
 }
 
-void	error_callback(int error, const char* description)
+void	error_callback(int error, const char *description)
 {
 	fprintf(stderr, "Error: %s - errno = %d\n", description, error);
 }
-
-// void	resize_callback(GLFWwindow* window, int width, int height)
-// {
-// 	(void)window;
-// 	printf("Resized : %d - %d\n", width, height);
-// 	ft_mat4x4_set_projection(data.cam.proj, (float[]){45, (float)data.width / data.height, .1f, 1000.0f});
-// 	glViewport(0, 0, width, height);
-// 	// TODO : Reset matrix
-// }
 
 int8_t	init_glfw(t_d *data)
 {
 	if (!glfwInit())
 		return (0);
-
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_SAMPLES, 3);
 	glfwSetErrorCallback(error_callback);
-
-	data->window = glfwCreateWindow(1600, 900, "Hello World", NULL, NULL);
+	data->window = glfwCreateWindow(1600, 900, "Scop", NULL, NULL);
 	if (!data->window)
 	{
 		glfwTerminate();
 		return (0);
 	}
 	glfwSetInputMode(data->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	// glfwSetFramebufferSizeCallback(data->window, resize_callback);
 	glfwMakeContextCurrent(data->window);
 	glfwGetWindowSize(data->window, &data->width, &data->height);
-	printf("Window size : %d - %d\n", data->width, data->height);
-	// glViewport(0, 0, data->width, data->height);
+	glClearColor(0.3, 0.3, 0.3, 1);
 	return (1);
 }
 
@@ -90,20 +78,20 @@ int8_t	init_all(t_d *data)
 		return (0);
 	if (!init_glfw(data))
 		return (0);
-
-	glEnable(GL_MULTISAMPLE); 
-	glEnable(GL_DEPTH_TEST); 
+	glEnable(GL_MULTISAMPLE);
+	glEnable(GL_DEPTH_TEST);
 	return (1);
 }
 
-int8_t    init_shaders(t_d *data, const char *frag, const char *vert)
+int8_t	init_shaders(t_d *data, const char *frag, const char *vert)
 {
 	if (!(data->vertex_sh = create_and_compile_shader(vert, GL_VERTEX_SHADER)))
 	{
 		dprintf(2, "Error while trying to create vertex shader.\n");
 		return (0);
 	}
- 	if (!(data->fragment_sh = create_and_compile_shader(frag, GL_FRAGMENT_SHADER)))
+	if (!(data->fragment_sh = create_and_compile_shader(frag,
+		GL_FRAGMENT_SHADER)))
 	{
 		dprintf(2, "Error while trying to create fragment shader.\n");
 		return (0);
@@ -111,13 +99,10 @@ int8_t    init_shaders(t_d *data, const char *frag, const char *vert)
 	return (1);
 }
 
-
-
 void	key_event(t_d *data, uint delta, t_obj *objs)
 {
-	// t_mat4x4 rotation;
-	uint			i;
-	static char *prev;
+	uint		i;
+	static char	*prev;
 
 	if (!prev)
 	{
@@ -135,83 +120,83 @@ void	key_event(t_d *data, uint delta, t_obj *objs)
 	ft_mat4x4_to_float_array(data->cam.view_f, data->cam.view);
 }
 
-void rotation_cam(t_d *data, uint delta)
+void	rotation_cam(t_d *data, uint delta)
 {
 	double			x;
 	double			y;
-	static double	last_x = 0;
-	static double	last_y = 0;
+	static t_vec2	last = {0, 0};
 	float			rotx;
 	t_vec3			tmp;
 
 	glfwGetCursorPos(data->window, &x, &y);
-	data->angle_y += (y - last_y) * delta / 1000;
-	rotx = (last_x - x);
-	data->dir = ft_vec3_rotate(data->dir, rotx * delta / 1000, (t_vec3){0, 1, 0});
+	data->angle_y += (y - last.y) * delta / 1000;
+	rotx = (last.x - x);
+	data->dir = ft_vec3_rotate(data->dir, rotx * delta / 1000,
+		(t_vec3){0, 1, 0});
 	tmp = (t_vec3){data->dir.x, 0, data->dir.z};
 	ft_vec3_normalize(&tmp);
 	if (data->angle_y < 90 && data->angle_y > -90)
-		data->dir = ft_vec3_rotate(tmp, data->angle_y, ft_vec3_cross(data->dir, (t_vec3){0, 1, 0}));
+		data->dir = ft_vec3_rotate(tmp, data->angle_y, ft_vec3_cross(data->dir,
+			(t_vec3){0, 1, 0}));
 	else
 		data->angle_y = data->angle_y >= 90 ? 90 : -90;
-	ft_mat4x4_set_look_at(data->cam.view, data->eye, ft_vec3_sub(data->eye, data->dir), data->cam.up);
-	last_y = y;
-	last_x = x;
+	ft_mat4x4_set_look_at(data->cam.view, data->eye, ft_vec3_sub(data->eye,
+		data->dir), data->cam.up);
+	last.y = y;
+	last.x = x;
 }
 
-t_obj*	get_all_obj(t_d *data, int ac, char **av)
+t_obj	*get_all_obj(t_d *data, int ac, char **av)
 {
-	t_obj*			objs;
-	uint i;
-	uint j;
+	t_obj	*objs;
+	uint	i;
+	uint	j;
 
 	if (ac == 1)
 		return (NULL);
 	objs = malloc(sizeof(t_obj) * (ac - 1));
 	data->texture_nbr = 1;
-	av++;
 	i = 0;
-	while (*av)
+	while (*(++av))
 	{
 		objs[i] = obj_parser_main(*av);
 		objs[i].scale = 1;
-		j = 0;
-		printf("obj #%d :\n\tname = %s\n", i, objs[i].name);
-		while (j < objs[i].mtl_nbr)
-		{
+		j = -1;
+		while (++j < objs[i].mtl_nbr)
 			if (objs[i].mtls[j].img_file)
 			{
 				objs[i].textures_nbr++;
 				data->texture_nbr++;
 			}
-			printf("\tmtls[%d].ref = %s\n", j, objs[i].mtls[j].ref);
-			printf("\tmtls[%d].img_file = %s\n", j, objs[i].mtls[j].img_file);
-			printf("\tmtls[%d].index = %d\n", j, objs[i].mtls[j].index);
-			j++;
-		}
 		if (!objs[i].error)
 			i++;
-		av++;
 	}
 	data->object_nbr = i;
 	return (objs);
 }
 
-void init_vbo(t_obj *obj)
+void	init_vbo(t_obj *obj)
 {
+	struct s_point 	point;
+
 	glBindBuffer(GL_ARRAY_BUFFER, obj->gl_buff.vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(struct s_point) * obj->indices_nbr, obj->points, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(struct s_point), (void*)0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(struct s_point) * obj->indices_nbr,
+		obj->points, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(struct s_point),
+		(GLvoid *)((void *)&point.vertex - (void *)&point));
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(struct s_point), (void*)sizeof(t_vec3));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(struct s_point),
+		(GLvoid *)((void *)&point.tex_vertex - (void *)&point));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(struct s_point), (void*)sizeof(t_vec3) + sizeof(t_vec2));
+	glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(struct s_point),
+		(GLvoid *)((void *)&point.rand - (void *)&point));
 	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(struct s_point), (void*)(sizeof(t_vec3) + sizeof(t_vec3)));
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(struct s_point),
+		(GLvoid *)((void *)&point.normal - (void *)&point));
 	glEnableVertexAttribArray(3);
 }
 
-void init_vao(t_obj *obj)
+void	init_vao(t_obj *obj)
 {
 	glGenVertexArrays(1, &obj->gl_buff.vao);
 	glGenBuffers(1, &obj->gl_buff.ebo);
@@ -219,35 +204,35 @@ void init_vao(t_obj *obj)
 	glBindVertexArray(obj->gl_buff.vao);
 	init_vbo(obj);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, obj->gl_buff.ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(obj->indices[0]) * obj->indices_nbr, obj->indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(obj->indices[0])
+		* obj->indices_nbr, obj->indices, GL_STATIC_DRAW);
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
-void get_glTexImage(uint *texs, uint *i_tex, t_d *data, char *file)
+void	get_glteximage(uint *texs, uint *i_tex, t_d *data, char *file)
 {
 	glBindTexture(GL_TEXTURE_2D, texs[*i_tex]);
 	create_image_from_png(data, *i_tex, file);
-	printf("i = %d %d %d\n", *i_tex, data->imgs[*i_tex].w, data->imgs[*i_tex].h);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, data->imgs[*i_tex].w, data->imgs[*
-	i_tex].h, 0, GL_BGRA, GL_UNSIGNED_BYTE, data->imgs[*i_tex].data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, data->imgs[*i_tex].w, data->imgs
+		[*i_tex].h, 0, GL_BGRA, GL_UNSIGNED_BYTE, data->imgs[*i_tex].data);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	(*i_tex)++;
 }
 
-uint *init_texs(t_d *data, t_obj *objs)
+uint	*init_texs(t_d *data, t_obj *objs)
 {
-	uint		*texs;
+	uint	*texs;
 	uint	i_tex;
 	uint	i;
 	uint	j;
 
 	data->imgs = malloc(sizeof(t_img) * data->texture_nbr);
-	texs = malloc(sizeof(uint) * data->texture_nbr); 
+	texs = malloc(sizeof(uint) * data->texture_nbr);
 	glGenTextures(data->texture_nbr, texs);
 	i = -1;
 	i_tex = 0;
-	get_glTexImage(texs, &i_tex, data, "./textures/bien.png");
+	get_glteximage(texs, &i_tex, data, "./textures/bien.png");
 	while (++i < data->object_nbr)
 	{
 		j = -1;
@@ -255,20 +240,22 @@ uint *init_texs(t_d *data, t_obj *objs)
 		{
 			objs[i].texOn = 1;
 			if (objs[i].mtls[j].img_file)
-				get_glTexImage(texs, &i_tex, data, objs[i].mtls[j].img_file);
+				get_glteximage(texs, &i_tex, data, objs[i].mtls[j].img_file);
 		}
 	}
-	return(texs);
+	return (texs);
 }
 
 void	init_uniform_data(t_d *data)
 {
 	data->eye = (t_vec3){0, 0, -10};
 	data->dir = (t_vec3){0, 0, -1};
-	data->cam.up = (t_vec3){0,1,0};
-	ft_mat4x4_set_look_at(data->cam.view, data->eye, ft_vec3_sub(data->eye, data->dir), data->cam.up);
+	data->cam.up = (t_vec3){0, 1, 0};
+	ft_mat4x4_set_look_at(data->cam.view, data->eye, ft_vec3_sub(data->eye,
+		data->dir), data->cam.up);
 	ft_mat4x4_to_float_array(data->cam.view_f, data->cam.view);
-	ft_mat4x4_set_projection(data->cam.proj, (float[]){45, (float)data->width / data->height, .1f, 1000.0f});
+	ft_mat4x4_set_projection(data->cam.proj, (float[]){45, (float)data->width
+		/ data->height, .1f, 1000.0f});
 	ft_mat4x4_to_float_array(data->cam.proj_f, data->cam.proj);
 	data->viewLoc = glGetUniformLocation(data->program, "view");
 	data->projLoc = glGetUniformLocation(data->program, "projection");
@@ -281,7 +268,6 @@ void	init_uniform_data(t_d *data)
 void	init_uniform_obj(t_d *data, t_obj *obj)
 {
 	obj->mid = ft_vec3_scalar_mult(ft_vec3_add(obj->min, obj->max), -0.5);
-	// ft_mat4x4_set_identity(obj->model);
 	ft_mat4x4_to_float_array(obj->model_f, obj->model);
 	obj->modelLoc = glGetUniformLocation(data->program, "model");
 	obj->texOnLoc = glGetUniformLocation(data->program, "texOn");
@@ -291,8 +277,8 @@ void	init_uniform_obj(t_d *data, t_obj *obj)
 void	draw_part_obj(t_obj *obj, uint *x, uint *texs)
 {
 	uint	y;
-	int	nb_face;
-	
+	int		nb_face;
+
 	y = 0;
 	while (y < obj->mtl_nbr)
 	{
@@ -308,7 +294,7 @@ void	draw_part_obj(t_obj *obj, uint *x, uint *texs)
 		else
 			glBindTexture(GL_TEXTURE_2D, texs[0]);
 		glMultiDrawElements(GL_TRIANGLE_FAN, &(obj->counts[obj->mtls[y].
-		index]), GL_UNSIGNED_INT, (const GLvoid * const *)(&(obj->offset[
+		index]), GL_UNSIGNED_INT, (const GLvoid *const *)(&(obj->offset[
 		obj->mtls[y].index])), nb_face);
 		y++;
 	}
@@ -316,15 +302,15 @@ void	draw_part_obj(t_obj *obj, uint *x, uint *texs)
 
 void	draw_obj(t_obj *obj, float angle, uint *x, uint *texs)
 {
-
 	glUniform1i(obj->texOnLoc, obj->texOn);
 	glUniform1i(obj->defTex, obj->textures_nbr == 0 ? 1 : 0);
 	glBindVertexArray(obj->gl_buff.vao);
 	ft_mat4x4_set_translation(obj->model, obj->pos);
-	ft_mat4x4_translate(obj->model, ft_vec3_scalar_mult(obj->mid, -1));
+	ft_mat4x4_translate(obj->model,
+		ft_vec3_scalar_mult(obj->mid, -obj->scale));
 	if (obj->rotOn)
 		ft_mat4x4_rotate(obj->model, angle, (t_vec3){0, 1, 0});
-	ft_mat4x4_translate(obj->model, obj->mid);
+	ft_mat4x4_translate(obj->model, ft_vec3_scalar_mult(obj->mid, obj->scale));
 	ft_mat4x4_scale(obj->model, (t_vec3){obj->scale, obj->scale, obj->scale});
 	ft_mat4x4_to_float_array(obj->model_f, obj->model);
 	glUniformMatrix4fv(obj->modelLoc, 1, GL_FALSE, obj->model_f);
@@ -332,7 +318,7 @@ void	draw_obj(t_obj *obj, float angle, uint *x, uint *texs)
 	{
 		glBindTexture(GL_TEXTURE_2D, texs[0]);
 		glMultiDrawElements(GL_TRIANGLE_FAN, obj->counts, GL_UNSIGNED_INT,
-			(const GLvoid * const *)obj->offset, obj->faces_nbr);
+			(const GLvoid *const *)obj->offset, obj->faces_nbr);
 	}
 	else
 		draw_part_obj(obj, x, texs);
@@ -349,22 +335,30 @@ void	init_frame(t_d *data, uint delta, t_obj *objs)
 	glUniformMatrix4fv(data->projLoc, 1, GL_FALSE, data->cam.proj_f);
 	glUniform1f(data->ambLoc, data->ambient);
 	glUniform4f(data->viewLoc, data->eye.x, data->eye.y, data->eye.z, 1.0);
-	glUniform4f(data->lightLoc, data->lightPos.x, data->lightPos.y, data->lightPos.z, 1.0);
-	glUniform4f(data->lightColorLoc, data->lightColor.x, data->lightColor.y, data->lightColor.z, 0.0);
+	glUniform4f(data->lightLoc, data->lightPos.x, data->lightPos.y,
+		data->lightPos.z, 1.0);
+	glUniform4f(data->lightColorLoc, data->lightColor.x, data->lightColor.y,
+		data->lightColor.z, 0.0);
+}
+
+void	update_time(t_d *data, t_obj *objs, uint *delta, struct timeval *tp)
+{
+	static uint				last = 0;
+
+	*delta = (tp->tv_sec * 1000 + tp->tv_usec / 1000) - last;
+	last = (tp->tv_sec * 1000 + tp->tv_usec / 1000);
+	init_frame(data, *delta, objs);
 }
 
 void	update_frame(t_d *data, t_obj *objs, uint *texs)
 {
-	uint						x;
+	uint					x;
 	uint					delta;
 	static struct timeval	tp = {0, 0};
-	static uint				last = 0;
-	size_t						i;
-	static float angle = 0;
+	size_t					i;
+	static float			angle = 0;
 
-	delta = (tp.tv_sec * 1000 + tp.tv_usec / 1000) - last;
-	last = (tp.tv_sec * 1000 + tp.tv_usec / 1000);
-	init_frame(data, delta, objs);
+	update_time(data, objs, &delta, &tp);
 	x = 1;
 	i = -1;
 	while (++i < data->object_nbr)
@@ -379,44 +373,29 @@ void	update_frame(t_d *data, t_obj *objs, uint *texs)
 	gettimeofday(&tp, NULL);
 }
 
-int main(int ac, char **av)
+int		main(int ac, char **av)
 {
 	t_d				data;
-	t_obj*			objs;
+	t_obj			*objs;
 	uint			*texs;
-	size_t				i;
+	size_t			i;
 
 	init_all(&data);
 	objs = get_all_obj(&data, ac, av);
-	if (!objs || objs[0].error)
-	{
-		printf("ERROR !\n");
-		exit(EXIT_FAILURE);
-	}
-	printf("OPengl version : %s\n", glGetString(GL_VERSION));
-	if (!init_shaders(&data, "./srcs/shaders/couleur2D.frag", "./srcs/shaders/couleur2D.vert"))
+	if (!objs || objs[0].error || !init_shaders(&data, "./srcs/shaders/couleur\
+2D.frag", "./srcs/shaders/couleur2D.vert") || !(
+	data.program = create_and_link_program(data.vertex_sh, data.fragment_sh)))
 		return (1);
-	if (!(data.program = create_and_link_program(data.vertex_sh, data.fragment_sh)))
-		return (2);
-	printf("nb = %zu\n", data.texture_nbr);
 	texs = init_texs(&data, objs);
 	init_uniform_data(&data);
-	i = 0;
-	printf("data.object_nbr = %zu\n", data.object_nbr);
-	while (i < data.object_nbr)
+	i = -1;
+	while (++i < data.object_nbr)
 	{
 		init_vao(&objs[i]);
 		init_uniform_obj(&data, &objs[i]);
-		i++;
 	}
-	glClearColor(0.3, 0.3, 0.3, 1);
-	data.drawing_mode = 0;
 	while (!glfwWindowShouldClose(data.window))
-	{
 		update_frame(&data, objs, texs);
-	}
-	printf("Closing window.\n");
-
 	glfwTerminate();
-	return 0;
+	return (0);
 }
--- srcs/shaders/couleur2D.frag
+++ srcs/shaders/couleur2D.frag
@@ -33,21 +33,28 @@ void main()
     vec4 lightDir = normalize(lightPos - gPos);
     // vec4 lightDir = normalize(lightPos - gl_FragCoord);
     float diff = dot(norm, lightDir);
+
+    if (diff < 0)
+        diff = 0;
+    // {
+    //     norm = -norm;
+    //     diff = dot(norm, lightDir);
+    // }
     vec4 diffuse = lightColor * diff;
 
     // specular
-    float specularStrength = 0.5;
+    float specularStrength = 1;
     vec4 viewDir = normalize(viewPos - gPos);
     // vec4 viewDir = normalize(viewPos - gl_FragCoord);
     vec4 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 5);
     vec4 specular = specularStrength * spec * lightColor;  
 
     // Couleur finale du pixel
     if (texOn == 1)
-    	aColor = texture(tex, gTexCoord) * (ambient + diffuse + specular);
+    	aColor = texture(tex, gTexCoord) * min(ambient + diffuse + specular, 1);
    	else
-   		aColor = gColor * (ambient + 0 + specular);
+   		aColor = gColor * min(ambient + diffuse + specular, 1);
    		// aColor = vec4(gl_PrimitiveID / (nb_face * 1.0), gl_PrimitiveID / (nb_face * 1.0), gl_PrimitiveID / (nb_face * 1.0), 0);
    		// aColor = vec4(gl_PrimitiveID / 2.0, gl_PrimitiveID / 2.0, gl_PrimitiveID / 2.0, 0);
 }
